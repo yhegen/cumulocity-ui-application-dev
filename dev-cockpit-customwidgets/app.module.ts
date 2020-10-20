642c5a12a4eb7ef46f1c5d283cfe988cdb479fab
yhegen <yhegenbarth@gmail.com>
2020-10-20 11:10:21 +0200

    dev environment for developing new widgets

---
+++ dev-cockpit-customwidgets/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule as NgRouterModule } from '@angular/router';
+import { UpgradeModule as NgUpgradeModule } from '@angular/upgrade/static';
+import { CoreModule, RouterModule } from '@c8y/ngx-components';
+import {
+  DashboardUpgradeModule,
+  UpgradeModule,
+  HybridAppModule,
+  UPGRADE_ROUTES
+} from '@c8y/ngx-components/upgrade';
+import { AssetsNavigatorModule } from '@c8y/ngx-components/assets-navigator';
+import { CockpitDashboardModule } from '@c8y/ngx-components/context-dashboard';
+import { ReportsModule } from '@c8y/ngx-components/reports';
+import { SensorPhoneModule } from '@c8y/ngx-components/sensor-phone';
+
+@NgModule({
+  imports: [
+    // Upgrade module must be the first
+    UpgradeModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(),
+    NgRouterModule.forRoot([...UPGRADE_ROUTES], { enableTracing: false, useHash: true }),
+    CoreModule.forRoot(),
+    AssetsNavigatorModule,
+    ReportsModule,
+    NgUpgradeModule,
+    DashboardUpgradeModule,
+    CockpitDashboardModule,
+    SensorPhoneModule
+  ]
+})
+export class AppModule extends HybridAppModule {
+  constructor(protected upgrade: NgUpgradeModule) {
+    super();
+  }
+}
